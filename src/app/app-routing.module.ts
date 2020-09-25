f26b513a10ae53459fb582ad0225603e51973e7e
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-25 16:58:45 +0300

    add basic routes to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RolesComponent } from './pages/roles/roles.component';
+import { UsersComponent } from './pages/users/users.component';
+import { WorkgroupsComponent } from './pages/workgroups/workgroups.component';
 
 const routes: Routes = [
   {
     path: '', component: LoginComponent,
   },
   {
-   path: 'dashboard', component: DashboardComponent
+    path: 'dashboard', component: DashboardComponent,
+    children: [
+      {
+        path: 'roles',
+         component: RolesComponent
+        //loadChildren: ()=> import('./pages/roles/roles.module').then(r=> r.RolesModule)
+      },
+      {
+        path: 'workgroups',
+        component: WorkgroupsComponent
+      },
+      {
+        path: 'users',
+        component: UsersComponent
+      }
+    ]
   }
 ];
 
