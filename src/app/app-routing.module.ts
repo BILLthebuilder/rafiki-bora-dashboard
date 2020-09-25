589f239b7f052dbbdb54a48311dd107de64432c5
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-25 14:00:02 +0300

    add routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent,
+  },
+  {
+   path: 'dashboard', component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
