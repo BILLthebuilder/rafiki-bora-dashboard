fe4150107fa3f500c21dbb6439c7e437ebbee366
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-25 11:24:02 +0300

    organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppImportsModule } from './app-imports-module/app-imports.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  AppImportsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
