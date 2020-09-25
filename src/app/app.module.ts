26cffa9966fe8157b2ca7bd1ebb9f0d95e6a86f4
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-25 14:17:17 +0300

    basic app configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppImportsModule } from './app-imports-module/app-imports.module';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
   AppImportsModule
