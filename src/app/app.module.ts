62373d07acd7d07ac41362c6271cae991e239187
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-25 16:59:34 +0300

    add default modules to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { AppComponent } from './app.component';
 import { AppImportsModule } from './app-imports-module/app-imports.module';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RolesComponent } from './pages/roles/roles.component';
+import { WorkgroupsComponent } from './pages/workgroups/workgroups.component';
+import { UsersComponent } from './pages/users/users.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -13,7 +20,11 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-  AppImportsModule
+    AppImportsModule,
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
 ],
   providers: [],
   bootstrap: [AppComponent]
