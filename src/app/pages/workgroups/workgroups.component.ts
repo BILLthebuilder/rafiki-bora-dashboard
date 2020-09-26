d65c71ea4d1a7b796b9d47a301d737fb17cca941
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-09-26 19:23:05 +0300

    populate the workgroup component

--- src/app/pages/workgroups/workgroups.component.ts
+++ src/app/pages/workgroups/workgroups.component.ts
@@ -1,15 +1,130 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Component, OnInit } from '@angular/core';
+
+export interface WorkGroup {
+  id: number;
+  name: string;
+  description: string;
+  createdBy: string;
+  approvedBy: string;
+  status: string;
+  dateCreated: string;
+}
+
+export interface Option{
+  value: string;
+  viewValue: string;
+}
+
+const workGroups: WorkGroup[] = [
+  {
+    id: 1,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 2,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 3,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 4,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 5,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 6,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 7,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 8,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 9,
+    name: 'admins',
+    description: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+];
 
 @Component({
   selector: 'app-workgroups',
   templateUrl: './workgroups.component.html',
-  styleUrls: ['./workgroups.component.scss']
+  styleUrls: ['./workgroups.component.scss'],
 })
 export class WorkgroupsComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'description',
+    'createdBy',
+    'approvedBy',
+    'status',
+    'dateCreated',
+  ];
 
-  constructor() { }
+  filters: Option[] = [
+    { value: 'approved', viewValue: 'Approved' },
+    { value: 'declined', viewValue: 'Declined' },
+  ];
 
-  ngOnInit(): void {
-  }
+  dataSource = workGroups;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
