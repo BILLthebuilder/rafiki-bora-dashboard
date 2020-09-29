356d4188af4966f4c56ef1c732ce2b487b96514a
ngechu <ngechu08@gmail.com>
2020-09-29 09:18:22 +0300

    add new roles routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,36 +2,44 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NewRoleComponent } from './pages/roles/new-role/new-role.component';
 import { RolesComponent } from './pages/roles/roles.component';
 import { UsersComponent } from './pages/users/users.component';
 import { WorkgroupsComponent } from './pages/workgroups/workgroups.component';
 
 const routes: Routes = [
   {
-    path: '', component: LoginComponent,
+    path: '',
+    component: LoginComponent,
   },
   {
-    path: 'dashboard', component: DashboardComponent,
+    path: 'dashboard',
+    component: DashboardComponent,
     children: [
       {
         path: 'roles',
-         component: RolesComponent
-        //loadChildren: ()=> import('./pages/roles/roles.module').then(r=> r.RolesModule)
+        component: RolesComponent,
+        children: [
+          {
+            path: 'new-role',
+            component: NewRoleComponent,
+          },
+        ],
       },
       {
         path: 'workgroups',
-        component: WorkgroupsComponent
+        component: WorkgroupsComponent,
       },
       {
         path: 'users',
-        component: UsersComponent
-      }
-    ]
-  }
+        component: UsersComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
