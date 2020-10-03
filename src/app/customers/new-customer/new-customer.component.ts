3090e78714d71fe6c4ce7066222e1d7c15fca5ce
ngechu <ngechu08@gmail.com>
2020-10-03 13:17:22 +0300

    Create new - role component

---
+++ src/app/customers/new-customer/new-customer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-customer',
+  templateUrl: './new-customer.component.html',
+  styleUrls: ['./new-customer.component.scss']
+})
+export class NewCustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/customers/new-customer/new-customer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NewCustomerRoutingModule } from './new-customer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NewCustomerRoutingModule
+  ]
+})
+export class NewCustomerModule { }
