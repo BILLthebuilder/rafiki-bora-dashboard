ec390e8fa2a44aac606a66e90f599e20befb4e72
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-10-03 12:24:40 +0300

    create merchant component

---
+++ src/app/merchants/merchants.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Merchant {
+  id: number;
+  name: string;
+  roleDescription: string;
+  createdBy: string;
+  approvedBy: string;
+  status: string;
+  dateCreated: string;
+}
+
+export interface Option {
+  value: string;
+  viewValue: string;
+}
+const merchants: Merchant[] = [
+  {
+    id: 1,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 2,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 3,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 4,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 5,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 6,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 7,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 8,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+  {
+    id: 9,
+    name: 'admins',
+    roleDescription: 'Administrators of stuff',
+    createdBy: 'John Doe',
+    approvedBy: 'John Doe',
+    status: 'Approved',
+    dateCreated: '26/09/2020',
+  },
+];
+@Component({
+  selector: 'app-roles',
+  templateUrl: './merchants.component.html',
+  styleUrls: ['./merchants.component.scss'],
+})
+export class MerchantsComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'merchantDescription',
+    'createdBy',
+    'approvedBy',
+    'status',
+    'dateCreated',
+  ];
+  filters: Option[] = [
+    { value: 'approved', viewValue: 'Approved' },
+    { value: 'declined', viewValue: 'Declined' },
+  ];
+
+  dataSource = merchants;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}
