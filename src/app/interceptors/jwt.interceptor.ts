a12de3c1eac1c1fc2ea964f0af5ca10f4c5827d6
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-10-13 20:13:52 +0300

    create request interceptors

---
+++ src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    // add JWT auth header if a user is logged in for API requests
+    const accessToken = localStorage.getItem('access_token');
+    const isApiUrl = request.url.startsWith('http://localhost:8080/');
+    if (accessToken && isApiUrl) {
+      request = request.clone({
+        setHeaders: { Authorization: `Bearer ${accessToken}` },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
---
+++ src/app/interceptors/unauthorized.interceptor.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class UnauthorizedInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err.status === 401) {
+          this.authService.clearLocalStorage();
+          this.router.navigate([''], {
+            queryParams: { returnUrl: this.router.routerState.snapshot.url },
+          });
+        }
+
+        if (!environment.production) {
+          console.error(err);
+        }
+        const error = (err && err.error && err.error.message) || err.statusText;
+        return throwError(error);
+      })
+    );
+  }
+}
