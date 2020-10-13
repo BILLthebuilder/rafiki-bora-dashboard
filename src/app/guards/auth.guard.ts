28a716cebe1be02e5ebf89741b8fb59c38a12e61
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-10-13 20:13:25 +0300

    create auth guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-else-return */
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) { }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.user$.pipe(
+      map((user) => {
+        if (user) {
+          return true;
+        } else {
+          this.router.navigate([''], {
+            queryParams: { returnUrl: state.url },
+          });
+          return false;
+        }
+      })
+    );
+  }
+}
