f098a5bd929f4d2167b1c710b74d55e03485113a
ngechu <ngechu08@gmail.com>
2020-10-23 14:44:36 +0300

    Add edit role functionality

--- src/app/roles/roles.module.ts
+++ src/app/roles/roles.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RolesRoutingModule } from './roles-routing.module';
 import { NewRoleComponent } from './new-role/new-role.component';
+import { EditRoleComponent } from './edit-role/edit-role.component';
 
 @NgModule({
-  declarations: [NewRoleComponent],
+  declarations: [NewRoleComponent, EditRoleComponent],
   imports: [CommonModule, RolesRoutingModule],
 })
 export class RolesModule {}
