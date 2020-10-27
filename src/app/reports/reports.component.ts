afe2ebae5e49831cb993ac8150d8090c0be4848b
ngechu <ngechu08@gmail.com>
2020-10-27 15:32:31 +0300

    Add filter by date fuctionality

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { AuthService } from '../services/auth.service';
 import { RafikiBoraService } from '../services/rafiki-bora.service';
@@ -22,11 +24,38 @@ export class ReportsComponent implements OnInit {
     'type',
     'date',
   ];
+  pipe: DatePipe;
+
+  filterForm = new FormGroup({
+    fromDate: new FormControl(),
+    toDate: new FormControl(),
+    status: new FormControl(),
+  });
+
+  get fromDate() {
+    return this.filterForm.get('fromDate').value;
+  }
+  get toDate() {
+    return this.filterForm.get('toDate').value;
+  }
+  get status() {
+    return this.filterForm.get('status').value;
+  }
 
   constructor(
     private authService: AuthService,
     private _rafikiBoraService: RafikiBoraService
-  ) {}
+  ) {
+    this.pipe = new DatePipe('en');
+    this.dataSource.filterPredicate = (data) => {
+      if (this.fromDate && this.toDate) {
+        return (
+          data.dateCreated >= this.fromDate && data.dateCreated <= this.toDate
+        );
+      }
+      return true;
+    };
+  }
 
   merchantFilters: any;
 
@@ -38,7 +67,7 @@ export class ReportsComponent implements OnInit {
     this._rafikiBoraService
       .getMerchantsData()
       .subscribe((data) => (this.merchantFilters = data));
-    this._rafikiBoraService.getAllTransactions().subscribe((data) => { 
+    this._rafikiBoraService.getAllTransactions().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data.transactions);
     });
   }
@@ -47,5 +76,11 @@ export class ReportsComponent implements OnInit {
     this._rafikiBoraService.getMerchantsTransaction(mid).subscribe((data) => {
       this.dataSource = new MatTableDataSource(data.transactions);
     });
+  }
+  applyFilter() {
+    this.dataSource.filter = `${Math.random()}`;
+  }
+  applySearchFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
