7490e4b8273f56f887246b894be9abce46e0612e
ngechu <ngechu08@gmail.com>
2020-10-27 10:39:12 +0300

    Fix filter by date

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
@@ -30,12 +32,39 @@ export class UsersComponent implements OnInit {
     { value: 'declined', viewValue: 'Declined' },
   ];
   public dataSource: any = [];
+  pipe: DatePipe;
+
+  filterForm = new FormGroup({
+    fromDate: new FormControl(),
+    toDate: new FormControl(),
+    status: new FormControl(),
+  });
+
+  get fromDate() {
+    return this.filterForm.get('fromDate').value;
+  }
+  get toDate() {
+    return this.filterForm.get('toDate').value;
+  }
+  get status() {
+    return this.filterForm.get('status').value;
+  }
 
   constructor(
     private _rafikiBoraService: RafikiBoraService,
     private router: Router,
     private _snackBar: MatSnackBar
-  ) {}
+  ) {
+    this.pipe = new DatePipe('en');
+    this.dataSource.filterPredicate = (data) => {
+      if (this.fromDate && this.toDate) {
+        return (
+          data.dateCreated >= this.fromDate && data.dateCreated <= this.toDate
+        );
+      }
+      return true;
+    };
+  }
 
   ngOnInit(): void {
     this._rafikiBoraService.getUserData().subscribe((data) => {
@@ -93,5 +122,8 @@ export class UsersComponent implements OnInit {
         this.ngOnInit();
       }
     );
+  }
+  applyFilterByDate() {
+    this.dataSource.filter = `${Math.random()}`;
   }
 }
