69faaabc009ccbde6089ced2fb518f77fac76e3f
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-05-08 21:10:22 +0300

    restructure chart component logic

--- src/app/merchants-dash/merchants-dash.component.ts
+++ src/app/merchants-dash/merchants-dash.component.ts
@@ -1,100 +1,118 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartDataSets } from 'chart.js';
-import { Label, Color } from 'ng2-charts';
+import {Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { RafikiBoraService } from '../services/rafiki-bora.service';
+import { ChartDataset,Chart,registerables } from 'chart.js';
+import { combineLatest, map } from 'rxjs';
+
 
 export interface Option {
   value: string;
   viewValue: string;
 }
-
 @Component({
   selector: 'app-merchants-dash',
   templateUrl: './merchants-dash.component.html',
   styleUrls: ['./merchants-dash.component.scss'],
 })
 export class MerchantsDashComponent implements OnInit {
-  sale;
-  deposit;
-  sendMoney;
-  recieveMoney;
+  sale = 500;
+  deposit = 400;
+  sendMoney = 560;
+  receiveMoney = 900;
   constructor(
     private authService: AuthService,
-    private _rafikiBoraService: RafikiBoraService
-  ) {}
+    private _rafikiBoraService: RafikiBoraService,
+  ) {
+    Chart.register(...registerables);
+  }
   filters: Option[] = [
     { value: 'rafiki1', viewValue: 'Rafiki1' },
     { value: 'rafiki2', viewValue: 'Rafiki2' },
   ];
-  lineChartData: ChartDataSets[] = [];
-
-  lineChartLabels: Label[] = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  lineChartOptions = {
-    responsive: true,
-  };
-  lineChartLegend = true;
-  lineChartPlugins = [];
-  lineChartType = 'line';
+  lineChartData: ChartDataset[] = [];
+  public chart: any;
+  dataset = [];
 
   ngOnInit(): void {
-    this._rafikiBoraService.getSale().subscribe((data) => {
-      this.sale = data;
-      const saleData = parseInt(data, 10);
-      this.lineChartData.push({
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, saleData, 0, 0],
-        label: 'Sales',
-        borderColor: '#A8385D',
-        backgroundColor: 'rgba(247, 251, 252, 0.2)',
-      });
-    });
-    this._rafikiBoraService.getDeposits().subscribe((data) => {
-      this.deposit = data;
-      const depositData = parseInt(data, 10);
-      this.lineChartData.push({
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, depositData, 0, 0],
-        label: 'Deposits',
-        backgroundColor: 'rgba(247, 251, 252, 0.2)',
-        borderColor: '#7AA3E5',
-      });
-    });
-    this._rafikiBoraService.getSendMoney().subscribe((data) => {
-      this.sendMoney = data;
-      const sendMoneyData = parseInt(data, 10);
-      this.lineChartData.push({
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, sendMoneyData, 0, 0],
-        label: 'Send Money',
-        backgroundColor: 'rgba(247, 251, 252, 0.2)',
-        borderColor: 'green',
-      });
-    });
-    this._rafikiBoraService.getReciveMoney().subscribe((data) => {
-      this.recieveMoney = data;
-      const recieveMoneyData = parseInt(data, 10);
-      this.lineChartData.push({
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, recieveMoneyData, 0, 0],
-        label: 'Recieve Money',
-        backgroundColor: 'rgba(247, 251, 252, 0.2)',
-        borderColor: 'green',
-      });
-    });
-    console.log(this.sale);
+    this.createChart();
   }
   isAdmin() {
     return this.authService.isAdmin();
   }
+  createChart() {
+    combineLatest({
+      sale: this._rafikiBoraService.getSale(),
+      deposits: this._rafikiBoraService.getDeposits(),
+      sendMoney: this._rafikiBoraService.getSendMoney(),
+      receiveMoney: this._rafikiBoraService.getReciveMoney(),
+    })
+      .pipe(
+        map((response) => {
+          this.sale = response.sale;
+          this.deposit = response.deposits;
+          this.sendMoney = response.sendMoney;
+          this.receiveMoney = response.receiveMoney;
+          const result: any[] = [];
+
+          result.push(
+            this.sale,
+            this.deposit,
+            this.sendMoney,
+            this.receiveMoney
+          );
+          return result;
+        })
+      )
+      .subscribe((data) => {
+        this.chart = new Chart('MyChart', {
+          type: 'line',
+          data: {
+            labels: [
+              'January',
+              'February',
+              'March',
+              'April',
+              'May',
+              'June',
+              'July',
+              'August',
+              'September',
+              'October',
+              'November',
+              'December',
+            ],
+            datasets: [
+              {
+                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, data[0], 0, 0],
+                label: 'Sales',
+                borderColor: '#A8385D',
+                backgroundColor: 'rgba(247, 251, 252, 0.2)',
+              },
+              {
+                data: [0, 0, 0, 0, 0, 0, 0, 0, data[1], 0, 0, 0],
+                label: 'Deposits',
+                backgroundColor: 'rgba(247, 251, 252, 0.2)',
+                borderColor: '#7AA3E5',
+              },
+              {
+                data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, data[2], 0],
+                label: 'Send Money',
+                backgroundColor: 'rgba(247, 251, 252, 0.2)',
+                borderColor: 'green',
+              },
+              {
+                data: [0, 0, 0, 0, 0, 0, data[3], 0, 0, 0, 0, 0],
+                label: 'Recieve Money',
+                backgroundColor: 'rgba(247, 251, 252, 0.2)',
+                borderColor: 'green',
+              },
+            ],
+          },
+          options: {
+            aspectRatio: 2.5,
+          },
+        });
+      });
+  }
 }
+
